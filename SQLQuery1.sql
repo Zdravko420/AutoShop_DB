1a678c60392564b3c053d2f52711e6fadb7059ba
Zdravko420 <43958501+Zdravko420@users.noreply.github.com>
2022-05-23 17:00:16 +0300

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,14 +1,13 @@
-/* Създаваме базата данни с име Авто Салон
 CREATE DATABASE CarDealership;
 */
 
-/* Tables - Създаваме таблиците, ще използваме модел-а за да направим релациите между различните таблици за да достъпваме различни данни, затова го правим Primary Key
+/* Tables - 
 CREATE TABLE AvailableCars(Model NVARCHAR(255) PRIMARY KEY, Quantity FLOAT)
 CREATE TABLE Sales(Model NVARCHAR(255) PRIMARY KEY, CustomOrders FLOAT, Sales FLOAT, Deliveries FLOAT, [Date] DATE)
 CREATE TABLE CarSpecs(Model NVARCHAR(255) PRIMARY KEY, YearOfManufacture INT, Horsepower INT, Price FLOAT)
 */
 
-/* Foreign Keys - Правим релациите между таблиците като използваме Model - Primary Key-a ни
+/* Foreign Keys 
 ALTER TABLE AvailableCars
 ADD FOREIGN KEY (Model) REFERENCES CarSpecs(Model);
 ALTER TABLE AvailableCars
@@ -17,7 +16,7 @@ ALTER TABLE Sales
 ADD FOREIGN KEY (Model) REFERENCES CarSpecs(Model);
 */
 
-/* Procedure 1 - Първата процедура изкарва общият брой на колите, които сме доставили на клиенти за период от време
+/* Procedure 1 
 CREATE PROCEDURE usp_TotalDeliveriesForPeriodOfTime(@Date1 DATE, @Date2 DATE)
 AS
 Select SUM(Deliveries) AS [Total Deliveries] FROM Sales 
@@ -26,7 +25,7 @@ GO
 EXEC usp_TotalDeliveriesForPeriodOfTime '2020-01-01', '2020-03-01';
 */
 
-/* Procedure 2 - Втората процедура изкарва общият брой продажби на колите за определен период от време
+/* Procedure 2
 CREATE PROCEDURE usp_TotalSalesForPeriodOfTime(@Date1 DATE, @Date2 DATE)
 AS
 Select SUM(Sales) AS [Total Sales] FROM Sales 
@@ -35,7 +34,7 @@ GO
 EXEC usp_TotalSalesForPeriodOfTime '2020-01-01', '2020-01-01'
 */
 
-/* Procedure 3 - Третата процедура изкарва всички налчни модели и броят им
+/* Procedure 3
 CREATE PROCEDURE usp_AvailableCars
 AS
 SELECT * FROM AvailableCars
@@ -43,7 +42,7 @@ GO;
 EXEC usp_AvailableCars
 */
 
-/* Procedure 4 - Четвъртата процедура изкарав всичките персонализирани поръчки на автомобили за период от време
+/* Procedure 4
 CREATE PROCEDURE usp_TotalCustomOrdersForPeriodOfTime(@Date1 DATE, @Date2 DATE)
 AS
 Select SUM(CustomOrders) AS [Total Sales] FROM Sales 
@@ -52,7 +51,7 @@ GO
 EXEC usp_TotalCustomOrdersForPeriodOfTime '2020-01-01', '2020-01-01'
 */
 
-/* Procedure 5 - Петата процедура изкарва общият брой продажби на автомобили
+/* Procedure 5
 CREATE PROCEDURE usp_TotalSales
 AS
 SELECT SUM(Sales) AS [Total Sales] FROM Sales
@@ -60,7 +59,7 @@ GO
 EXEC usp_TotalSales;
 */
 
-/* Procedure 6 - Шестата процедура изкарва всички нови бройки автомобили за период от време
+/* Procedure 6
 CREATE PROCEDURE usp_NewCars(@Date1 DATE, @Date2 DATE)
 AS
 SELECT Sum([New Cars]) AS [New Cars] FROM Sales
@@ -69,7 +68,7 @@ GO
 EXECE usp_NewCars '2020-01-01', '2020-03-01'
 */
 
-/* Procedure 7 - Седмата процедура изкарва колите, които не са успели да се продадат за период от време
+/* Procedure 7
 CREATE PROCEDURE usp_DroppedCars(@Date1 DATE, @Date2 DATE)
 AS
 SELECT (AvailableCars.Quantity - Sales.Sales) AS [Dropped Cars] FROM Sales 
@@ -79,7 +78,7 @@ GO
 EXEC usp_DroppedCars '2020-01-01', '2020-01-01';
 */
 
-/* Procedure 8 - Осмата процедура изкарва общият приход за определен период от време
+/* Procedure 8
 CREATE PROCEDURE usp_TotalRevenue(@Date1 DATE, @Date2 DATE)
 AS
 SELECT SUM(Sales.Sales * CarSpecs.Price) FROM Sales
@@ -90,7 +89,7 @@ EXEC usp_TotalRevenue '2020-01-01', '2020-8-01'
 */
 
 
-/* Procedure 9 - Деветата процедура изкарва общият разход за определен период от време
+/* Procedure 9
 CREATE PROCEDURE usp_TotalExpenses(@Date1 DATE, @Date2 DATE)
 AS
 SELECT SUM((Sales.Sales * CarSpecs.Price) * 0.20) AS [Total Expenses] FROM Sales
